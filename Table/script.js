15f9c92d7ac3824679c0ba585f4d4cc5c5b34210
radigurev <radi.gurev@gmail.com>
2022-03-02 08:45:19 +0200

    working animations

--- Table/script.js
+++ Table/script.js
@@ -1,22 +1,73 @@
 const buttons= [...document.querySelectorAll("[buttons-grade]")]
 const students_div=[...document.getElementsByName("name")]
-
+const inputs=[...document.querySelectorAll("[inputs]")]
+const divs=[...document.querySelectorAll("[all-button-divs]")]
+const inputDivs=[...document.getElementsByClassName("hidden")]
+const returnButton=[...document.getElementsByClassName("return-button")]
 var students=new Array
 
 students_div.forEach(s => students.push(s.textContent))
 
-console.log(students)
+console.log(returnButton)
 
 buttons.forEach((b,index) =>{
     b.addEventListener("click",e => {
         if(b.classList.contains("selected")){
             b.classList.remove("selected")
         }else {
-            b.classList.add("selected")
+            OpenTextBox(index)
         }
+        
     })
 })
 
+returnButton.forEach((b,index)=>{
+    b.addEventListener("click",e =>{
+        CloseTextBox(index)
+    })
+})
+
+function OpenTextBox(inx) {
+    let index=Math.floor(inx/5)
+       
+        setTimeout(function(){
+            divs[index].classList.add("hidden")
+        },100)
+
+        inputs[index].value=buttons[inx].value
+
+        setTimeout(function(){
+            divs[index].classList.add("hide")
+        },1100)
+        
+        setTimeout(function() {
+            inputDivs[index].classList.remove("hide")
+        },2000)
+
+        setTimeout(function() {
+            inputDivs[index].classList.remove("hidden")
+        },2100)
+        
+       
+}
+
+function CloseTextBox(index) {    
+    setTimeout(function(){
+        inputDivs[index].classList.add("hidden")
+    },100)
+
+    setTimeout(function(){
+        inputDivs[index].classList.add("hide")
+    },1100)
+
+    setTimeout(function() {
+        divs[index].classList.remove("hide")
+    },2000)
+
+    setTimeout(function() {
+        divs[index].classList.remove("hidden")
+    },2100)
+}
 
 function getGrades() {
     let loop=0
@@ -26,17 +77,20 @@ function getGrades() {
    console.log(grade)
   buttons.forEach((b) => {
       while(loop!==4){
-        
-        if(b.classList.contains("selected")){
-            finalList[index]=new Student(students[index],b.value)
-            grade = 0
-            index++
-              loop++
-          }
+          if(b.classList.contains("selected"))
+          grade = b.value
+         console.log(grade)
+         loop++
       }
+      if(grade!==0){
+        finalList[index]=new Student(students[index],grade)
+        console.log(finalList)
+        index++
+      }
+        grade = 0
        loop=0
+       
   })
-  console.log(finalList)
 }
 
 class Student {
