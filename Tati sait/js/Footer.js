ffc7e63f1994ce7d4e4529b3fafda973efa93c25
radigurev <radi.gurev@gmail.com>
2022-03-04 10:09:54 +0200

    added my father site

---
+++ Tati sait/js/Footer.js
@@ -0,0 +1,25 @@
+
+        function setupFooter() {
+            var pageHeight = $('.tm-site-header-container').height() + $('footer').height() + 100;
+
+            var main = $('.tm-main-content');
+
+            if($(window).height() < pageHeight) {
+                main.addClass('tm-footer-relative');
+            }
+            else {
+                main.removeClass('tm-footer-relative');   
+            }
+        }
+
+    
+        $(function(){
+
+            setupFooter();
+
+            $(window).resize(function(){
+                setupFooter();
+            });
+
+            $('.tm-current-year').text(new Date().getFullYear());  // Update year in copyright           
+        });
